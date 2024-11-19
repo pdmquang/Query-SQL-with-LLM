ef93bbc143bdd633c2700962f773e17358d07196
DESKTOP-OGDKKJ9\USER <PhamQuang@activants.com>
2024-11-19 20:48:03 +0800

    Remove comments, handle errors, add README.md

--- query_by_SQL.py
+++ query_by_SQL.py
@@ -9,49 +9,21 @@ model = ChatOllama(
     model="llama3:8b"
 )
 
-question = "Get payment limit of this user id = INLABLB"
+question = "Get payment limit of this user id = INLABLB in table CT_USER_LEVEL"
 # question = "How many rows in this table?"
 
-# cnxn = pyodbc.connect("Driver={SQL Server Native Client 11.0};"
-#                       "Server=localhost;"
-#                       "Database=SVT_UAT;"
-#                       "Trusted_Connection=yes;")
-
-# db = SQLDatabase.from_uri("sqlite:///mydb.db")
-# connectionString = "mssql+pyodbc://localhost:1433/SVT_UAT?driver=ODBC Driver 17 for SQL Server;Trusted_Connection=yes;"
-connectionString = "mssql+pyodbc:///?odbc_connect='DRIVER={ODBC Driver 17 for SQL Server};SERVER=DESKTOP-OGDKKJ9;DATABASE=SVT_UAT;Trusted_Connection=yes;"
-# connection_string = (
-#     "Driver={ODBC Driver 17 for SQL Server};"
-#     "Server=localhost;"
-#     "Database=your_database_name;"
-#     "Trusted_Connection=yes;"
-# )
-
 conn = 'DRIVER={ODBC Driver 17 for SQL Server};SERVER=localhost;DATABASE=SVT_UAT;Trusted_Connection=yes;'
-cnxn = pyodbc.connect(conn)
-
-# connectionString = "mssql+pymssql://localhost/SVT_UAT;Trusted_Connection=yes;"
 db_engine = create_engine(f"mssql+pyodbc:///?odbc_connect={conn}")
-
-# with db_engine.connect() as connection:
-    # result = connection.execute("SELECT * FROM CT_USER_LEVEL")
-    # print(result)
-# db = SQLDatabase(db_engine)
-# print(db.get_table_names())
-
-# Step 2: Create an SQLAlchemy engine from the pyodbc connection
-# engine = create_engine("mssql+pyodbc://localhost:1433/SVT_UAT?driver=ODBC+Driver+17+for+SQL+Server;Trusted_Connection=yes;")
-# engine = create_engine('mssql+pyodbc:///?odbc_connect=' + cnxn.connection_string)
-
-# Step 3: Initialize LangChain's SQLDatabase with the engine
-# db = SQLDatabase.from_uri(engine.url)
 
 # Create a SQLDatabase instance
 db = SQLDatabase(db_engine)
-agent_executor = create_sql_agent(model, db=db, verbose=False, handle_parsing_errors=True)
+agent_executor = create_sql_agent(model, db=db, verbose=True, 
+                                  agent_executor_kwargs={"handle_parsing_errors": True},)
 
-# print(agent_executor.invoke(question))
-
-answers = agent_executor.invoke(question)
-
-print("Answer: ", answers)
+try:
+    # Invoke the agent executor with the question
+    print(f"********ANSWER**********: \n{agent_executor.invoke(question)}")
+    # print(langchain.globals.get_verbose())
+except ValueError as e:
+    # Handle parsing errors specifically
+    print(f"An error occurred**********: {e}")
